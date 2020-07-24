5271b4433a816f6d321472c38e8eb80e45b306b9
박동건 <hbd9425@gmail.com>
2020-07-24 19:42:33 +0900

    update: fix contents && add to-top button

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function openSlideMenu() {
-  document.getElementById("side-menu").style.width = "250px";
+  document.getElementById("side-menu").style.width = "100%";
 }
 
 function closeSlideMenu() {
+  // x버튼 눌렀을 때
   document.getElementById("side-menu").style.width = "0";
 }
